4c64b8f3110666d5643aebfd29733f843c4657e1
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-06-20 21:27:30 +0400

    Initial commit

---
+++ lackage.hs
@@ -0,0 +1,118 @@
+import qualified Data.ByteString.Lazy as L
+
+import Data.List
+import Data.Text.Lazy (unpack)
+import Data.Text.Lazy.Encoding (decodeUtf8With)
+import Data.Text.Encoding.Error (lenientDecode)
+
+import Distribution.Package
+import Distribution.PackageDescription
+import Distribution.PackageDescription.Parse
+import Distribution.Text
+
+import Codec.Compression.GZip (decompress, compress)
+import Codec.Archive.Tar       as Tar hiding (unpack)
+import Codec.Archive.Tar.Entry as Tar
+
+
+import System.Environment
+import System.Directory
+
+
+
+----------------------------------------------------------------
+-- Main
+----------------------------------------------------------------
+
+upload :: FilePath -> FilePath -> IO ()
+upload tarball dir = do
+  -- Read tarball
+  bs <- case tarball of
+          "-" -> L.getContents
+          _   -> L.readFile tarball
+  -- Retrieve cabal file
+  let cabal = getCabal $ Tar.read $ decompress bs
+      descr = getDescription cabal
+  -- Build pathes
+  let pkg              = package $ packageDescription descr
+      PackageName name = pkgName    pkg
+      version          = pkgVersion pkg
+      -- Pathes
+      dirPath   = name ++ "/" ++ display version ++ "/"
+      tarDir    = dir ++ "/" ++ dirPath
+      tarPath   = tarDir ++ name ++ "-" ++ display version ++ ".tar.gz"
+      indexPath = dirPath ++ name ++ ".cabal"
+  -- Append file to index
+  appendToTar (dir ++ "/" ++ "00index.tar.gz") indexPath cabal
+  -- Write file to directory
+  createDirectoryIfMissing True tarDir
+  L.writeFile tarPath bs
+
+
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ["upload", sdist, dir] -> upload sdist dir
+    _                      -> error "Unknow command"
+
+
+
+----------------------------------------------------------------
+-- Helpers
+----------------------------------------------------------------
+
+-- | Write compressed tarball
+writeTar :: FilePath -> [Entry] -> IO ()
+writeTar path = L.writeFile path . compress . Tar.write
+
+-- | Read compressed tarball
+readTar :: FilePath -> IO [Entry]
+readTar path = do
+  bs <- L.readFile path
+  return $ foldEntries (:) [] (error . show)
+         $ Tar.read
+         $ decompress bs
+
+-- | Append file to the gzip compressed tarball
+appendToTar :: FilePath -> FilePath -> L.ByteString -> IO ()
+appendToTar tar nm content = do
+  -- Create entry for tarball
+  let entry = case toTarPath False nm of
+                Left  e    -> error e
+                Right path -> fileEntry path content
+  f <- doesFileExist tar
+  case f of
+    -- No file.
+    False -> writeTar tar [entry]
+    -- Append to existing file
+    True  -> do
+      es <- readTar tar
+      -- Check for duplication
+      case find ((==nm) . entryPath) es of
+        Just _  -> error "Duplicate file in the tarball"
+        Nothing -> return ()
+      -- Write everything
+      writeTar tar (entry : es)
+
+
+
+-- | Retrieve cabal file from tarball as bytestring
+getCabal :: Show a => Entries a -> L.ByteString
+getCabal Done = error "No cabal file"
+getCabal (Next entry rest)
+  | reverse (take 6 $ reverse $ entryPath entry) == ".cabal" =
+      case entryContent entry of
+        NormalFile bs _ -> bs
+        _  -> error   "Cabal file must be normal file"
+
+  | otherwise                                                = getCabal rest
+getCabal (Fail s) = error $ "Invalid tarball: " ++ show s
+
+-- | Extract description from package
+getDescription :: L.ByteString -> GenericPackageDescription
+getDescription cabal =
+  case parsePackageDescription $ unpack $ decodeUtf8With lenientDecode cabal of
+    ParseOk _ x -> x
+    err         -> error $ "Invalid cabal file: " ++ show err
